acad5eaa658d332345a16b44fb7c4f7fca2adfcb
Ryosato6386 <ryo.sato@cellpromote.biz>
2023-05-26 13:57:35 +0900

    削除ボタンの作成

--- web.app/src/main/java/com/example/demo/controller/WebController.java
+++ web.app/src/main/java/com/example/demo/controller/WebController.java
@@ -2,8 +2,11 @@ package com.example.demo.controller;
 
 import java.time.LocalDateTime;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class WebController {
 
 	@GetMapping(value = "/")
